39ec5841a849d3dd51da65ff00c673860c2e3ede
Николай Зайко <kolya-zayko@rambler.ru>
2017-12-01 14:18:38 +0200

    commit

--- src/by/tms/database/UsersConnector.java
+++ src/by/tms/database/UsersConnector.java
@@ -10,12 +10,17 @@ public class UsersConnector {
     private static final String PASSWORD = "root";
     private static Connection connection;
 
+
+
     public static Connection getConnection() {
         if (connection == null) {
             try {
+                Class.forName("com.mysql.jdbc.Driver");
                 connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 return connection;
             } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
