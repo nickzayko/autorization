8ace05392d4b61d6b9a95050fed988dfc7f4f528
Николай Зайко <kolya-zayko@rambler.ru>
2017-12-02 14:04:48 +0200

    commit

--- src/by/tms/servlets/Autorization.java
+++ src/by/tms/servlets/Autorization.java
@@ -17,6 +17,8 @@ public class Autorization extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+            int ifUserExist = 0;
+            Users tmpUser = new Users();
             String login = req.getParameter("login");
             String password = req.getParameter("password");
             ArrayList<Users> users = new ArrayList<Users>();
@@ -28,13 +30,19 @@ public class Autorization extends HttpServlet {
                 }
                 for (Users user : users) {
                     if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
-                        req.getServletContext().setAttribute("name", user.getName());
-                        req.getServletContext().setAttribute("age", user.getAge());
-                        req.getServletContext().setAttribute("login", user.getLogin());
-                        req.getServletContext().setAttribute("password", user.getPassword());
-                        req.getServletContext().getRequestDispatcher("/goodAutorization.jsp").forward(req, resp);
+                       ifUserExist++;
+                       tmpUser = user;
                     }
                 }
+            }
+            if (ifUserExist != 0){
+                req.getServletContext().setAttribute("name", tmpUser.getName());
+                req.getServletContext().setAttribute("age", tmpUser.getAge());
+                req.getServletContext().setAttribute("login", tmpUser.getLogin());
+                req.getServletContext().setAttribute("password", tmpUser.getPassword());
+                req.getServletContext().getRequestDispatcher("/goodAutorization.jsp").forward(req, resp);
+            } else {
+                req.getServletContext().getRequestDispatcher("/badAutorization.jsp").forward(req, resp);
             }
         } catch (IOException e) {
             req.getServletContext().getRequestDispatcher("/badAutorization.jsp").forward(req, resp);
